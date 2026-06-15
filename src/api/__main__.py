7c698cc69dd61c259defaf0be1e85c56ed2ce53b
ksredkin <ksredkin0968@gmail.com>
2026-06-15 13:39:44 +0300

    add get and update server settings endpoints

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -3,12 +3,14 @@ from fastapi.responses import JSONResponse
 import uvicorn
 from src.common.core.config import API_HOST, API_PORT
 from src.api.services.process_service import ProcessService
+from src.api.services.properties_service import PropertiesService
 
 app = FastAPI(title="Minecraft Server Manager", description="API для управления Minecraft сервером.")
 process_service = ProcessService()
+properties_service = PropertiesService()
 
 @app.post("/start", description="Запустить сервер.")
-async def start():
+def start() -> JSONResponse:
     result = process_service.start()
     if result:
         return JSONResponse({"success": True}, 200)
@@ -16,7 +18,7 @@ async def start():
         raise HTTPException(500, {"success": False})
 
 @app.post("/stop", description="Остановить сервер.")
-async def stop():
+def stop() -> JSONResponse:
     result = process_service.stop()
     if result:
         return JSONResponse({"success": True}, 200)
@@ -24,7 +26,7 @@ async def stop():
         raise HTTPException(500, {"success": False})
 
 @app.post("/restart", description="Перезапустить сервер.")
-async def restart():
+def restart() -> JSONResponse:
     result = process_service.restart()
     if result:
         return JSONResponse({"success": True}, 200)
@@ -32,10 +34,26 @@ async def restart():
         raise HTTPException(500, {"success": False})
 
 @app.get("/status", description="Получить текущий статус работы сервера.")
-async def status():
+def status() -> JSONResponse:
     status = process_service.status()
     if status:
         return JSONResponse({"success": True, "data": {"status": status}}, 200)
+    else:
+        raise HTTPException(500, {"success": False})
+    
+@app.get("/properties", description="Получить настройки сервера.")
+def properties() -> JSONResponse:
+    properties = properties_service.get_properties()
+    if properties:
+        return JSONResponse({"success": True, "data": {"properties": properties}}, 200)
+    else:
+        raise HTTPException(500, {"success": False})
+
+@app.put("/properties/{property}", description="Обновить настройку сервера.")
+def update_property(property: str, value: str) -> JSONResponse:
+    success = properties_service.update_property(property, value)
+    if success:
+        return JSONResponse({"success": True, "data": {property: value}}, 200)
     else:
         raise HTTPException(500, {"success": False})
 
---
+++ src/api/services/properties_service.py
@@ -0,0 +1,45 @@
+from src.common.core.config import SERVER_PATH
+import os
+
+class PropertiesService:
+    def __init__(self, server_path: str = SERVER_PATH) -> None:
+        if not server_path:
+            raise ValueError("В конфиге не установлен путь к серверу.")
+        
+        properties_file_path = SERVER_PATH.rstrip("/").rstrip("\\") + "/" + "server.properties"
+
+        if not os.path.exists(properties_file_path):
+            raise RuntimeError("Не найден файл настройки сервера.")
+
+        self.properties_file_path = properties_file_path
+
+    def get_properties(self) -> dict[str, str]:
+        with open(self.properties_file_path, "r") as f:
+            lines = f.readlines()
+
+        properties = {}
+
+        for line in lines:
+            if "#" != line[0]:
+                key, value = line.split("=", 1)
+                properties[key] = value.rstrip("\n")
+
+        return properties
+
+    def update_property(self, property: str, value: str) -> bool:
+        with open(self.properties_file_path, "r") as f:
+            properties = f.readlines()
+
+        is_changed = False
+
+        for i, line in enumerate(properties):
+            if line[:len(property)+1] == property+"=":
+                properties[i] = property + "=" + value  + "\n"
+                is_changed = True
+                break
+
+        if is_changed:
+            with open(self.properties_file_path, "w") as f:
+                f.writelines(properties)
+
+        return is_changed
